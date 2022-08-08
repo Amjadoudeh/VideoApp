8f5f78af4f609f8491e17b2d4dd05ff8d2ad845c
Amjad Oudeh <74506206+Amjadoudeh@users.noreply.github.com>
2022-08-08 15:40:47 +0200

    Create a conditional query button

--- VideoApp/Components /QueryTag.swift
+++ VideoApp/Components /QueryTag.swift
@@ -1,13 +1,26 @@
 import SwiftUI
 
 struct QueryTag: View {
+    var query: String
+    var isSelected: Bool
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        Text(query)
+            .font(.subheadline)
+            .fontWeight(isSelected ? .bold : .semibold)
+            .foregroundColor(isSelected ? .black : .white)
+            .padding()
+            .background(isSelected ? .white : .gray)
+            .cornerRadius(25)
+        
     }
 }
 
 struct QueryTag_Previews: PreviewProvider {
     static var previews: some View {
-        QueryTag()
+        QueryTag(query: "Cars", isSelected: true)
+            .preferredColorScheme(.dark)
     }
 }
+
