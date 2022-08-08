693c7d31c434da2ce83c15c51cd260e9f275f246
Amjad Oudeh <74506206+Amjadoudeh@users.noreply.github.com>
2022-08-08 14:53:47 +0200

    First commit

---
+++ VideoApp/VideoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoAppApp.swift
+//  VideoApp
+//
+//  Created by Amjad Oudeh on 08.08.22.
+//
+
+import SwiftUI
+
+@main
+struct VideoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
