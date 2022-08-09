d19af6d8c2ee8de47395e583ee995d3c277df241
Amjad Oudeh <74506206+Amjadoudeh@users.noreply.github.com>
2022-08-09 15:07:06 +0200

    Create the HTTP request and handle the response

--- VideoApp/VideoManager.swift
+++ VideoApp/VideoManager.swift
@@ -10,6 +10,46 @@ enum Query: String, CaseIterable {
 enum Explore: String {
     case Explore
 }
+//MARK: Class for the HTTP request
+class VideoManager: ObservableObject {
+    @Published private(set) var videos: [Video] = []
+    @Published private var selectedQuery: Query = Query.Cars {
+        didSet {
+            Task.init {
+                await findVideos(topic: selectedQuery)
+            }
+        }
+    }
+    
+    init(){
+        Task.init {
+            await findVideos(topic: selectedQuery)
+        }
+    }
+    
+    func findVideos(topic: Query) async {
+        do {
+            guard let url = URL(string: "https://api.pexels.com/videos/search?query=\(topic)&per_page=15") else { fatalError("Missing URL")}
+            
+            var urlRequest = URLRequest(url: url)
+            urlRequest.setValue("563492ad6f91700001000001171cc72abc8c4b37b2c09d0ebd0de313", forHTTPHeaderField: "Authorization")
+            
+            let (data , response) = try await URLSession.shared.data(for: urlRequest)
+            
+            guard ( response as? HTTPURLResponse)?.statusCode == 200 else { fatalError("Error while fetching data")}
+            
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            let decodedData = try decoder.decode(ResponseBody.self, from: data)
+            // clear it before the next response
+            self.videos = []
+            self.videos = decodedData.videos
+ 
+        } catch {
+            print("Error fetching data from the server: \(error)")
+        }
+    }
+}
 
 //MARK: ResponseBody
 struct ResponseBody: Decodable {
